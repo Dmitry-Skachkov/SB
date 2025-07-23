c7f6cdb9898d707ad9c82a9002bb3715d13945c0
Dmitry Skachkov <d.g.skachkov@gmail.com>
2025-07-23 14:29:25 -0500

    Update run.sh

--- Examples/Example_2/QE_CBS/run.sh
+++ Examples/Example_2/QE_CBS/run.sh
@@ -1,3 +1,3 @@
 
-mpiexec -enable-x -ppn 8 pw.x     < scf.in  > scf.out
-mpiexec -enable-x -ppn 8 pwcond.x < cond.in > cond.out
+mpiexec -ppn 8 pw.x     < scf.in  > scf.out
+mpiexec -ppn 8 pwcond.x < cond.in > cond.out
